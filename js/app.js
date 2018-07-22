8727fb42b802c79ef26bb9b86303b1d0fa1470fe
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2018-07-21 23:21:22 -0300

    removendo comentario desnecessario

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
  //array com as 8 imagens dos cards
  let listaImagem = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt',
 					'fa-cube','fa-leaf','fa-bicycle','fa-bomb'];
@@ -30,14 +27,7 @@ function shuffle(array) {
 }
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * funcao que inicia o processo apos o load da pagina html
  */
 window.onload = function initElement() {
     //gerar card randomicos no jogo
